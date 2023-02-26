66a27025a7e517932fc93831ef35d32fd9a84354
Lorenzo Panieri <gokk99@gmail.com>
2023-02-26 01:34:15 +0100

    module header

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stoej_core
-Base module for Stoej JUCE extensions
+
+stoej is a library of various utilities that help with the development of audio
+plugins with the JUCE framework. It depends on JUCE to function.
---
+++ stoej_core.h
@@ -0,0 +1,16 @@
+/*
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:                     stoej_core
+    vendor:                 stoej
+    version:                0.1.0
+    name:                   stoej_core
+    description:            Extensions for the JUCE framework
+
+    license:                GPL3 / commercial
+    minimumCppStandard:     20
+
+END_JUCE_MODULE_DECLARATION
+
+*/
